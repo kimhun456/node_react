3d5291621d778fb22fe78f1bd435c7561e7db720
HyunJae <kimhun456@gmail.com>
2017-01-03 02:18:48 +0900

    router memo js modify

--- server/routes/memo.js
+++ server/routes/memo.js
@@ -1,25 +1,60 @@
 import express from 'express';
-import memo from '../models/memo';
-import mongoose from "mongoose";
+import Memo from '../models/memo';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/', (req,res) =>{
-    
+/* 
+    WRITE MEMO: POST /api/memo
+    BODY SAMPLE: { contents: "sample "}
+    ERROR CODES
+        1: NOT LOGGED IN
+        2: EMPTY CONTENTS
+*/
+router.post('/', (req, res) => {
+
+    if (typeof req.session.logininfo === 'undefined') {
+        return res
+            .status(403)
+            .json({error: 'NOT LOGGED IN', code: 1});
+    }
+
+    if (typeof req.body.contents !== 'string') {
+        return res
+            .status(400)
+            .json({error: 'EMPTY CONTENTS', code: 2});
+    }
+
+    if (req.body.contents === '') {
+        return res
+            .status(400)
+            .json({error: 'EMPTY CONTENTS', code: 2});
+    }
+
+    let memo = new Memo({writer: req.session.logininfo.username, contents: req.body.contents});
+
+    memo.save((err) => {
+        if (err) 
+            throw err;
+        return res.json({success: true});
+    });
+
 });
 
+router.put('/:id', (req, res) => {});
 
-router.put('/:id', (req,res) =>{
+router.delete('/:id', (req, res) => {});
 
-});
-
-
-router.delete('/:id', (req,res) =>{
-
-});
-
-
-router.get('/', (req,res) =>{
+router.get('/', (req, res) => {
+    Memo
+        .find()
+        .sort({'_id': -1})
+        .limit(6)
+        .exec((err, memos) => {
+            if (err) 
+                throw err;
+            res.json(memos);
+        });
 
 });
 
